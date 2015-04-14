f584f2f41fe7bcc045c7ec76e1bb896728828a97
Egor Starostin <egorst@gmail.com>
2015-04-14 23:30:54 +0300

    trivial changes

--- oradash.go
+++ oradash.go
@@ -1,40 +1,46 @@
 package main
+
 import (
-    "database/sql"
-    "fmt"
-//    "strings"
-//    "os"
-    "github.com/gizak/termui"
-    _ "github.com/mattn/go-oci8"
+	"database/sql"
+	"fmt"
+	//    "strings"
+	//    "os"
+	"github.com/gizak/termui"
+	_ "github.com/mattn/go-oci8"
 )
 
 func main() {
-    err := termui.Init()
-    db, err := sql.Open("oci8","tools/catch22");
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    defer db.Close()
-    if err = testSelect(db); err != nil {
-        fmt.Println(err)
-        return
-    }
+	err := termui.Init()
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer termui.Close()
+	db, err := sql.Open("oci8", "tools/catch22")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
+	if err = testSelect(db); err != nil {
+		fmt.Println(err)
+		return
+	}
+	<-termui.EventCh()
 }
 
 func testSelect(db *sql.DB) error {
-    rows, err := db.Query("select name from gv$database")
-    if err != nil {
-        return err
-    }
-    defer rows.Close()
-    for rows.Next() {
-        var namedb string
-        if err = rows.Scan(&namedb); err != nil {
-            return err
-        }
-        got := fmt.Sprintf("'%s'",namedb)
-        fmt.Println("connected to ",got)
-    }
-    return nil
+	rows, err := db.Query("select name from gv$database")
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var namedb string
+		if err = rows.Scan(&namedb); err != nil {
+			return err
+		}
+		got := fmt.Sprintf("'%s'", namedb)
+		fmt.Println("connected to ", got)
+	}
+	return nil
 }
