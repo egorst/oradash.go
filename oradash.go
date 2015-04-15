965116a9cd386fb66de817b6c6adbfb22c6d9dda
Egor Starostin <egorst@gmail.com>
2015-04-15 23:43:09 +0300

    playing with grid

--- oradash.go
+++ oradash.go
@@ -40,7 +40,13 @@ func testSelect(db *sql.DB) error {
 			return err
 		}
 		got := fmt.Sprintf("'%s'", namedb)
-		fmt.Println("connected to ", got)
+		par := termui.NewPar("connected to " + got)
+		par.Height = 4
+		par.Border.Label = "DB Summary"
+		termui.Body.AddRows(termui.NewRow(termui.NewCol(6, 0, par)))
+		termui.Body.Align()
+		termui.Render(termui.Body)
+		//fmt.Println("connected to ", got)
 	}
 	return nil
 }
