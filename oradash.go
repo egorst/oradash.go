8c40997abc1ff8aad5f2a4f5abee42ae865920bf
Egor Starostin <egorst@gmail.com>
2015-04-27 11:20:13 +0300

    top sessions from v$active_session_history

--- oradash.go
+++ oradash.go
@@ -29,24 +29,54 @@ func main() {
 }
 
 func testSelect(db *sql.DB) error {
-	rows, err := db.Query("select name from gv$database")
+	rows, err := db.Query(`SELECT * FROM (
+    SELECT /*+ LEADING(a) USE_HASH(u) */
+      COUNT(*) totalseconds
+      , ROUND((COUNT(*) / 300), 1) AAS
+      , LPAD(ROUND(RATIO_TO_REPORT(COUNT(*)) OVER () * 100)||'%',5,' ') percent
+      , session_id
+      , TO_CHAR(MIN(sample_time), 'YYYY-MM-DD HH24:MI:SS') first_seen
+      , TO_CHAR(MAX(sample_time), 'YYYY-MM-DD HH24:MI:SS') last_seen
+    FROM
+        (SELECT
+             a.*
+           , TO_CHAR(CASE WHEN session_state = 'WAITING' THEN p1 ELSE null END, '0XXXXXXXXXXXXXXX') p1hex
+           , TO_CHAR(CASE WHEN session_state = 'WAITING' THEN p2 ELSE null END, '0XXXXXXXXXXXXXXX') p2hex
+           , TO_CHAR(CASE WHEN session_state = 'WAITING' THEN p3 ELSE null END, '0XXXXXXXXXXXXXXX') p3hex
+        FROM gv$active_session_history a) a
+      , dba_users u
+    WHERE
+        a.user_id = u.user_id (+)
+    AND sample_time BETWEEN systimestamp-5/1440 AND systimestamp
+    GROUP BY session_id
+    ORDER BY TotalSeconds DESC, session_id
+)
+WHERE ROWNUM <= 5
+`)
 	if err != nil {
+		panic(err)
 		return err
 	}
 	defer rows.Close()
+	var got string
 	for rows.Next() {
-		var namedb string
-		if err = rows.Scan(&namedb); err != nil {
+		var totalseconds string
+		var aas int
+		var percent string
+		var session_id string
+		var first_seen string
+		var last_seen string
+		if err = rows.Scan(&totalseconds, &aas, &percent, &session_id, &first_seen, &last_seen); err != nil {
 			return err
 		}
-		got := fmt.Sprintf("'%s'", namedb)
-		par := termui.NewPar("connected to " + got)
-		par.Height = 4
-		par.Border.Label = "DB Summary"
-		termui.Body.AddRows(termui.NewRow(termui.NewCol(6, 0, par)))
-		termui.Body.Align()
-		termui.Render(termui.Body)
-		//fmt.Println("connected to ", got)
+		got += fmt.Sprintf("%10s\t%6d\t%10s\t%10s\t%14s\t%14s\n", totalseconds, aas, percent, session_id, first_seen, last_seen)
 	}
+	par := termui.NewPar(got)
+	par.Height = 4
+	par.Border.Label = "Top 5 Sessions"
+	termui.Body.AddRows(termui.NewRow(termui.NewCol(12, 0, par)))
+	termui.Body.Align()
+	termui.Render(termui.Body)
+	//fmt.Println("connected to ", got)
 	return nil
 }
